3b4e724489185f50adb8e80e9493d5533b38a353
Kirill Romanov <romanov.k48@gmail.com>
2024-09-09 13:25:34 +0700

    add printing text to parent thread
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>

--- 1.1/src/main/java/ru/nsu/romanov/Main.java
+++ 1.1/src/main/java/ru/nsu/romanov/Main.java
@@ -4,6 +4,7 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        new Thread(() -> IntStream.range(0, 10).forEach(i -> System.out.println(i + " text"))).start();
+        new Thread(() -> IntStream.range(0, 10).forEach(i -> System.out.println(i + " text of child"))).start();
+        IntStream.range(0, 10).forEach(i -> System.out.println(i + "  of parent"));
     }
 }

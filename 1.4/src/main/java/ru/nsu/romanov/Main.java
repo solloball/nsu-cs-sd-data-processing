e794cfac5cb366f6c4a5a906a1153d000c420143
Kirill Romanov <romanov.k48@gmail.com>
2024-09-11 12:06:51 +0700

    change task
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>

--- 1.4/src/main/java/ru/nsu/romanov/Main.java
+++ 1.4/src/main/java/ru/nsu/romanov/Main.java
@@ -6,22 +6,16 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         var thread = new Thread(() -> {
-            while(true) {
-                if (Thread.currentThread().isInterrupted()) {
-                    System.out.println("Interrupted");
-                    break;
-                }
+            while(!Thread.currentThread().isInterrupted()) {
                 System.out.println("Some text!");
             }
         });
 
-        System.out.println("Start children thread");
         thread.start();
         try {
             TimeUnit.SECONDS.sleep(2);
         } catch (InterruptedException ignored) {
         }
-        System.out.println("Interrupt children thread");
         thread.interrupt();
     }
 }

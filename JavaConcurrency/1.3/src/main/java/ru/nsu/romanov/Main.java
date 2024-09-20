e9bb71e70d8e4c8733070da7cd04ec11c260da88
Kirill Romanov <romanov.k48@gmail.com>
2024-09-20 22:38:55 +0700

    remake task
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>

--- JavaConcurrency/1.3/src/main/java/ru/nsu/romanov/Main.java
+++ JavaConcurrency/1.3/src/main/java/ru/nsu/romanov/Main.java
@@ -1,17 +1,38 @@
 package ru.nsu.romanov;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        new Thread(() -> printStrings(List.of("1 string", "1 hello", "1 test"))).start();
-        new Thread(() -> printStrings(List.of("2 wow", "2 name", "2 java"))).start();
-        new Thread(() -> printStrings(List.of("3 class", "3 text", "3 thread"))).start();
-        new Thread(() -> printStrings(List.of("4 god", "4 pc", "4 word"))).start();
 
+
+        class Writer implements Runnable {
+            private final List<String> str;
+
+            public Writer(List<String> str) {
+                this.str = new ArrayList<>(str);
+            }
+
+            @Override
+            public void run() {
+                System.out.println(str);
+            }
+        }
+
+        List<String> list = new ArrayList<>();
+        list.add("Test");
+        list.add("Task");
+        new Thread(new Writer(list)).start();
+        list.set(0, "Main");
+        list.set(1, "Mask");
+        new Thread(new Writer(list)).start();
+        list.set(0, "Toto");
+        list.set(1, "Todo");
+        new Thread(new Writer(list)).start();
+        list.set(0, "java");
+        list.set(1, "language");
+        new Thread(new Writer(list)).start();
     }
 
-    private static void printStrings(List<String> strings) {
-        strings.forEach(System.out::println);
-    }
 }

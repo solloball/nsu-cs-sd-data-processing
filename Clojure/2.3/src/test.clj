fc7c2c1ab6e9faceb26fd7f684f908ee535e6805
Kirill Romanov <romanov.k48@gmail.com>
2024-12-19 12:43:53 +0700

    fix
    Signed-off-by: Kirill Romanov <romanov.k48@gmail.com>

--- Clojure/2.3/src/test.clj
+++ Clojure/2.3/src/test.clj
@@ -1,1 +1,26 @@
-(ns test)
+(ns test
+  (:require [clojure.test :refer :all]
+            [main :refer [myFilter, numDivisors]]))
+
+(deftest testBasicBig
+  (testing
+    (is (= (myFilter even? (range 500))
+           (filter even? (range 500)))
+        )
+    )
+  )
+
+(deftest testPerformanceBig
+  (testing
+    (let [test-data (range)]
+      (println "My Filter: ")
+      (time (doall (take 10000 (filter #(> (numDivisors %) 10) test-data))))
+      (println "Default filter: ")
+      (time (doall (take 10000 (myFilter #(> (numDivisors %) 10) test-data)))))))
+
+(deftest testLazy
+  (testing
+    (let [infinite-coll (range)
+          result (myFilter even? infinite-coll)]
+      (is (= (take 10 result)
+             (take 10 (filter even? infinite-coll)))))))
